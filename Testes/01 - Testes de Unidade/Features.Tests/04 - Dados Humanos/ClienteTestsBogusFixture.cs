7bc61c4236675c17e663f538d4e452b9b19d18ce
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2022-03-27 11:54:18 -0300

    Converted to file-scoped the namespaces

--- Testes/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
+++ Testes/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -6,71 +6,70 @@ using Bogus.DataSets;
 using Features.Clientes;
 using Xunit;
 
-namespace Features.Tests
+namespace Features.Tests;
+
+[CollectionDefinition(nameof(ClienteBogusCollection))]
+public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>
+{ }
+
+public class ClienteTestsBogusFixture
 {
-    [CollectionDefinition(nameof(ClienteBogusCollection))]
-    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>
-    {}
-
-    public class ClienteTestsBogusFixture
+    public Cliente GerarClienteValido()
     {
-        public Cliente GerarClienteValido()
-        {
-            return GerarClientes(1, true).FirstOrDefault();
-        }
+        return GerarClientes(1, true).FirstOrDefault();
+    }
 
-        public IEnumerable<Cliente> ObterClientesVariados()
-        {
-            var clientes = new List<Cliente>();
+    public IEnumerable<Cliente> ObterClientesVariados()
+    {
+        var clientes = new List<Cliente>();
 
-            clientes.AddRange(GerarClientes(50, true).ToList());
-            clientes.AddRange(GerarClientes(50, false).ToList());
+        clientes.AddRange(GerarClientes(50, true).ToList());
+        clientes.AddRange(GerarClientes(50, false).ToList());
 
-            return clientes;
-        }
+        return clientes;
+    }
 
-        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
-        {
-            var genero = new Faker().PickRandom<Name.Gender>();
+    public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+    {
+        var genero = new Faker().PickRandom<Name.Gender>();
 
-            //var email = new Faker().Internet.Email("eduardo","pires","gmail");
-            //var clientefaker = new Faker<Cliente>();
-            //clientefaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
+        //var email = new Faker().Internet.Email("eduardo","pires","gmail");
+        //var clientefaker = new Faker<Cliente>();
+        //clientefaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
 
-            var clientes = new Faker<Cliente>("pt_BR")
-                .CustomInstantiator(f => new Cliente(
-                    Guid.NewGuid(), 
-                    f.Name.FirstName(genero),
-                    f.Name.LastName(genero),
-                    f.Date.Past(80,DateTime.Now.AddYears(-18)), // Irá retornar data de nascimento de até 80 anos atrás mas todas maiores de 18 anos
-                    "",
-                    ativo,
-                    DateTime.Now))
-                .RuleFor(c=>c.Email, (f,c) => 
-                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+        var clientes = new Faker<Cliente>("pt_BR")
+            .CustomInstantiator(f => new Cliente(
+                Guid.NewGuid(),
+                f.Name.FirstName(genero),
+                f.Name.LastName(genero),
+                f.Date.Past(80, DateTime.Now.AddYears(-18)), // Irá retornar data de nascimento de até 80 anos atrás mas todas maiores de 18 anos
+                "",
+                ativo,
+                DateTime.Now))
+            .RuleFor(c => c.Email, (f, c) =>
+                  f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return clientes.Generate(quantidade);
-        }
+        return clientes.Generate(quantidade);
+    }
 
-        public Cliente GerarClienteInvalido()
-        {
-            var genero = new Faker().PickRandom<Name.Gender>();
+    public Cliente GerarClienteInvalido()
+    {
+        var genero = new Faker().PickRandom<Name.Gender>();
 
-            var cliente = new Faker<Cliente>("pt_BR")
-                .CustomInstantiator(f => new Cliente(
-                    Guid.NewGuid(),
-                    f.Name.FirstName(genero),
-                    f.Name.LastName(genero),
-                    f.Date.Past(1, DateTime.Now.AddYears(1)),
-                    "",
-                    false,
-                    DateTime.Now));
+        var cliente = new Faker<Cliente>("pt_BR")
+            .CustomInstantiator(f => new Cliente(
+                Guid.NewGuid(),
+                f.Name.FirstName(genero),
+                f.Name.LastName(genero),
+                f.Date.Past(1, DateTime.Now.AddYears(1)),
+                "",
+                false,
+                DateTime.Now));
 
-            return cliente;
-        }
+        return cliente;
+    }
 
-        public void Dispose()
-        {
-        }
+    public void Dispose()
+    {
     }
 }
