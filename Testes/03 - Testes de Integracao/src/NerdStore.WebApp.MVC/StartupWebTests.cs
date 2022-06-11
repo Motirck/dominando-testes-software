696547b846fd684afc1287bff99372fc8a738da3
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2022-06-11 16:56:50 -0300

    UsuarioTests in progress

--- Testes/03 - Testes de Integracao/src/NerdStore.WebApp.MVC/StartupWebTests.cs
+++ Testes/03 - Testes de Integracao/src/NerdStore.WebApp.MVC/StartupWebTests.cs
@@ -1,26 +1,18 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using NerdStore.Catalogo.Application.AutoMapper;
 using NerdStore.Catalogo.Data;
 using NerdStore.Vendas.Data;
 using NerdStore.WebApp.MVC.Data;
-using NerdStore.WebApp.MVC.Models;
 using NerdStore.WebApp.MVC.Security;
 using NerdStore.WebApp.MVC.Setup;
-using System.Text;
 
 namespace NerdStore.WebApp.MVC
 {
     public class StartupWebTests : IStartup
     {
         public IConfiguration Configuration { get; }
-        private readonly string _apiName;
-        private readonly string _apiDescription;
-        private readonly string _apiVersion;
 
         public StartupWebTests(IWebHostEnvironment hostEnvironment)
         {
@@ -31,10 +23,6 @@ namespace NerdStore.WebApp.MVC
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
-
-            _apiName = Configuration["ApiName"];
-            _apiDescription = Configuration["ApiDescription"];
-            _apiVersion = Configuration["ApiVersion"];
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -94,13 +82,6 @@ namespace NerdStore.WebApp.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            // Swagger
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint($"{_apiVersion}/swagger.json", $"{_apiName} {_apiVersion}");
-            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
