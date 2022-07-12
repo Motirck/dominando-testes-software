edbaaa173dbe389a4cad58eee13c2ab71129865c
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2022-07-12 15:36:57 -0300

    Configs of helpers in progress

--- Testes/04 - BDD e Testes de Automação/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
+++ Testes/04 - BDD e Testes de Automação/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -1,6 +1,42 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
 namespace NerdStore.BDD.Tests.Config
 {
-    public class SeleniumHelper
+    public class SeleniumHelper : IDisposable
     {
+        public IWebDriver WebDriver;
+        public readonly ConfigurationHelper Configuration;
+        public WebDriverWait Wait;
+
+        public SeleniumHelper(Browser browser, ConfigurationHelper configuration, bool headless = true)
+        {
+            Configuration = configuration;
+            WebDriver = WebDriverFactory.CreateWebDriver(browser, Configuration.WebDrivers, headless);
+            WebDriver.Manage().Window.Maximize();
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(30));
+        }
+
+        public string ObterUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void IrParaUrl(string url)
+        {
+            WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public void ClicarLinkPorTexto(string linkText)
+        {
+            var link = Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText(linkText)));
+            link.Click();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
